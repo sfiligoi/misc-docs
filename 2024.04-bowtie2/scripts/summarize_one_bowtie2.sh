9d280ee0da001bfaf31ecda07bf46c05fc6b8feb
Igor Sfiligoi <isfiligoi@ucsd.edu>
2024-07-26 14:23:42 -0700

    Fix script

--- 2024.04-bowtie2/scripts/summarize_one_bowtie2.sh
+++ 2024.04-bowtie2/scripts/summarize_one_bowtie2.sh
@@ -6,5 +6,5 @@
 
 t2=`./parse_one_bowtie2.sh "$1" 2 |tail -1 | awk '{printf("%.1f\n",$2/$1)}'`
 t3=`./parse_one_bowtie2.sh "$1" 3 |tail -1 | awk '{printf("%.1f\n",$2/$1)}'`
-t4=`python3 -c "print(\"%.1f, %2i%%\"%(${t2}+${t3},${t2}*100/(${t2}+${t3})))"`
+t4=`python3 -c "print(\"%.1f, %.0f%%\"%(${t2}+${t3},${t2}*100/(${t2}+${t3})))"`
 echo "$2, ${t2}, ${t3}, ${t4}"
